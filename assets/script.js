8abc8ba677c322824706bf37b78df63f4ce793c6
joycesylo <joycesylo@airbears2-10-142-40-122.airbears2.1918.berkeley.edu>
2015-04-27 10:58:10 -0700

    init commit

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+
+$("#connect").click(function() {
+	$("#modal-container").show()
+	});
+
+
+$("#button").click(function() {
+	$("#modal-container").hide()
+	});
+
+$("#modal-overlay").click(function() {
+	$("#modal-container").hide()
+	});
+
+$("#image").click(function() {
+	window.location.href="about.html"
+	});
+
+
+});
